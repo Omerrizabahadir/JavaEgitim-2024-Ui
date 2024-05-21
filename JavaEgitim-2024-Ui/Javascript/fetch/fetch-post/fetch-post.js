09d70f51927422a8f148651afd401ab006720399
Omerrizabahadir <omrbahadir@gmail.com>
2024-05-21 20:51:32 +0300

    (POST process) Json type data with POST(save) method using javascript

---
+++ JavaEgitim-2024-Ui/Javascript/fetch/fetch-post/fetch-post.js
@@ -0,0 +1,46 @@
+
+
+//POST ile datayı kydedeceğiz
+
+const postForm=document.getElementById('post-form');
+
+postForm.addEventListener('submit', function(event){
+   
+event.preventDefault();  //varsayılanı engelle event geldiğinde alert vermesi için
+
+    const title = document.getElementById('title').value;
+    console.log(title)
+
+    const body = document.getElementById('body').value;
+    console.log(body)
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({   //string ifadeleri JSON'a çevirecek
+
+            title:title,    //key, value. Eğer bu ikisi aynı olursa sadece key olarak yazabilirsin
+            body:body,
+            userId:1,  
+        })
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Oluşturulan gönderi:',data);
+
+         // İsteği başarıyla tamamladığınızı kullanıcıya alert(uyarı vererek)bildirebilirsiniz
+        alert('Gönderi başarıyla oluşturuldu!')
+
+        //Formu temizle
+
+        postForm.requestFullscreen();
+
+
+    })
+    .catch(error =>{
+        console.error('Error:', error);
+    })
+    console.log('Gönderi oluşturuldu')
+});
